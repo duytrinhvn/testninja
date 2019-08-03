01546f8deeab88def9951627e8b77ad70df4d69d
Trinh Duy <khacduyy97@gmail.com>
2019-08-03 11:51:31 -0400

    Add test cases for Math class and HtmlFormatter class

---
+++ TestNinja.UnitTests.Me/HtmlFormatterTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Me
+{
+    [TestFixture]
+    class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ReturnBoldedHtml()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+            // Specific
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+
+            // General
+            Assert.That(result, Does.StartWith("<strong>"));
+            Assert.That(result, Does.EndWith("</strong>"));
+            Assert.That(result, Does.Contain("abc"));
+        }
+    }
+}
---
+++ TestNinja.UnitTests.Me/MathTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Me
+{
+    [TestFixture]
+    class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new Math();
+
+            var result = math.Add(299, 100);
+
+            Assert.That(result, Is.EqualTo(399));
+        }
+
+        [Test]
+        public void Max_WhenFirstArgumentIsGreater_ReturnFirstArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(188, 97);
+
+            Assert.That(result, Is.EqualTo(188));
+        }
+
+        [Test]
+        public void Max_WhenSecondArgumentIsGreater_ReturnSecondArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(28, 37);
+
+            Assert.That(result, Is.EqualTo(37));
+        }
+
+        [Test]
+        public void Max_WhenArgumentsAreEqual_ReturnOneArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(28, 28);
+
+            Assert.That(result, Is.EqualTo(28));
+        }
+    }
+}
