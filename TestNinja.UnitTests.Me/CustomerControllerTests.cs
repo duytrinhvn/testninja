fa4e6a57a7b583e9f1aa2fd369492e00c772bc42
Trinh Duy <khacduyy97@gmail.com>
2019-08-05 17:16:06 -0400

    Test GetCustomer function in CustomerController

---
+++ TestNinja.UnitTests.Me/CustomerControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Me
+{
+    [TestFixture]
+    class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFoundClass()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOkClass()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(4);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
