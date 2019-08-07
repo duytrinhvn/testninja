06705d403465f13f14108da6a06834dc792ac1f4
Trinh Duy <khacduyy97@gmail.com>
2019-08-07 13:27:58 -0400

    Test ErrorLogger class

---
+++ TestNinja.UnitTests.Me/ErrorLoggerTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests.Me
+{
+    [TestFixture]
+    class ErrorLoggerTest
+    {
+        private ErrorLogger _logger;
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new ErrorLogger();
+        }
+
+        [Test]
+        public void Log_WhenCalled_SetLastErrorProperty()
+        {
+            _logger.Log("a");
+
+            Assert.That(_logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            Assert.That( () => _logger.Log(error), Throws.ArgumentNullException);
+        }
+    }
+}
